b28af2bb39d077bac0d60c7236f3b802031757ae
Brian Bousman <brian.bousman@boeing.com>
2016-02-19 22:05:46 -0600

    added animation to splash screen

--- app/src/main/java/com/project/bousman/unitfun/SplashActivity.java
+++ app/src/main/java/com/project/bousman/unitfun/SplashActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,8 @@ public class SplashActivity extends AppCompatActivity {
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
 
         setContentView(R.layout.activity_splash);
+        TextView titleView = (TextView)findViewById(R.id.title_text);
+        titleView.setAlpha(0);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -30,5 +34,10 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+        ImageView graphic = (ImageView)findViewById(R.id.uxGraphic);
+        graphic.animate().rotation(360).setDuration(SPLASH_DISPLAY_LENGTH).start();
+        titleView.animate().alpha(1).setDuration(SPLASH_DISPLAY_LENGTH).start();
+
     }
 }
